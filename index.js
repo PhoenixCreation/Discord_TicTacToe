dd3c72f3d42c3780599fbcb9f0a1db25db8c36f8
phoenix <phs28052001@gmail.com>
2021-01-18 10:37:04 +0530

    Added server count to presence

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ import handleCommand from "./command.js";
 client.on("ready", () => {
   // Bot is online
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(
+    `Number of servers the bot is in: ${client.guilds.cache.array().length}`
+  );
+  client.user
+    .setActivity(
+      `People fight with each other in ${
+        client.guilds.cache.array().length
+      } servers.`,
+      { type: "WATCHING" }
+    )
+    .then((presence) =>
+      console.log(`Activity set to ${presence.activities[0].name}`)
+    )
+    .catch(console.error);
 });
 
 client.on("message", handleCommand);
